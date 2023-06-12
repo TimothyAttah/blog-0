2dd13f08bd190f1a5df492b4066e4550ec516b52
Timothy Attah <timothyattah733@gmail.com>
2023-06-12 22:45:16 +0100

    Created global styles

---
+++ frontend/src/styles/GlobalStyles.ts
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    max-width: 1500px;
+    width: 100%;
+    margin: 0 auto;
+  }
+
+  html {
+    font-size: 10px;
+
+    @media screen and (max-width: 400px){
+      font-size: 8px;
+    }
+  }
+
+  h1, h2, h3, h4, h5, h6, p {
+    padding: 0;
+    margin: 0;
+  }
+
+  a {
+    color: #000;
+    text-decoration: none;
+  }
+
+  ul, li {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  button {
+    border: none;
+    outline: none;
+
+    :focus {
+      outline: none;
+    }
+  }
+`;
