b6ceefaa732d26c4d81f0d6ce8244d4143279610
Timothy Attah <timothyattah733@gmail.com>
2023-06-25 13:40:31 +0100

    Thanking God for everything

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -30,6 +30,7 @@ function App() {
 
       <h2>Thank you Lord for your mercy. Thank you Jesus...</h2>
       <p>I'm really grateful for your mercy oh Lord. </p>
+      <p>THANK YOU JESUS</p>
 
       <ul>
         <li>Create your website</li>
